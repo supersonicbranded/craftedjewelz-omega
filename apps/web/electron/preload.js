01ae6e860d0824709a4b0c09edd308d1de9cb1c3
supersonicbranded <info@supersonicbranded.com>
2025-09-12 13:24:48 -0400

    Update preload.js

--- apps/web/electron/preload.js
+++ apps/web/electron/preload.js
@@ -1,9 +1,6 @@
-const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge } = require("electron");
 
 contextBridge.exposeInMainWorld("craftedjewelz", {
-  ping: () => "pong",
-  getVersion: () => ipcRenderer.invoke("get-version"),
-  saveFile: (data) => ipcRenderer.invoke("save-file", data),
-  openFile: () => ipcRenderer.invoke("open-file"),
-  checkForUpdates: () => ipcRenderer.invoke("check-for-updates")
+  version: "1.0.0",
+  ping: () => "pong"
 });
