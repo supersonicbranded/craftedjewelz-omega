ca7f66689a1e05b8f8dbd6cad2c74d3aa27068ac
supersonicbranded <info@supersonicbranded.com>
2025-09-12 14:21:31 -0400

    Update main.js

--- apps/web/electron/main.js
+++ apps/web/electron/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 
 function createWindow() {
@@ -6,26 +6,40 @@ function createWindow() {
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js")
-    }
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false
+    },
+    icon: path.join(__dirname, "../build/icons/icon.png")
   });
 
-  win.loadFile(path.join(__dirname, "../dist/index.html"));
+  if (process.env.NODE_ENV === "development") {
+    win.loadURL("http://localhost:5173");
+  } else {
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
 }
 
 app.whenReady().then(() => {
   createWindow();
+
   app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
   });
 });
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
-});
-
+  if (process.platform !== "darwin") {
+    app.quit();
   }
 });
+
+ipcMain.handle("ping", async () => {
+  return "pong";
+});
+
 
 
 
