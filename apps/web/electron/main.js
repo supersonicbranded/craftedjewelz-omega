270a2c8e2a50dc87d523efa3a8e7c890d30584f4
supersonicbranded <info@supersonicbranded.com>
2025-09-13 00:20:04 -0400

    Update main.js

--- apps/web/electron/main.js
+++ apps/web/electron/main.js
@@ -1,43 +1,38 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const path = require("path");
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1200,
+    width: 1280,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-      contextIsolation: true,
-      nodeIntegration: false
+      preload: path.join(__dirname, 'preload.js'),
     },
-    icon: path.join(__dirname, "../build/icons/icon.png")
+    icon: path.join(__dirname, '../apps/web/build/icons/appicon.png')
   });
 
-  if (process.env.NODE_ENV === "development") {
-    win.loadURL("http://localhost:5173");
+  // Load your frontend app (from build or dev server)
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:3000');
   } else {
-    win.loadFile(path.join(__dirname, "../dist/index.html"));
+    win.loadFile(path.join(__dirname, '../apps/web/dist/index.html'));
   }
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  app.on("activate", () => {
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
 });
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
-});
-
-ipcMain.handle("ping", async () => {
-  return "pong";
 });
 
 
