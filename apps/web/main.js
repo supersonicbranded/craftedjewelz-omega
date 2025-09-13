994ee82f3642e6b87f6b6cf5198af9ba94390144
supersonicbranded <info@supersonicbranded.com>
2025-09-13 00:50:25 -0400

    Create main.js

---
+++ apps/web/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+    icon: path.join(__dirname, "build", "appicon2.png"),
+  });
+
+  // Load your app (adjust URL or file path as needed)
+  if (process.env.NODE_ENV === "development") {
+    win.loadURL("http://localhost:3000");
+  } else {
+    win.loadFile(path.join(__dirname, "dist", "index.html"));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
