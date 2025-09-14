d8cdf2d7c3d4b85099325ccf8b4b60a51053a603
supersonicbranded <info@supersonicbranded.com>
2025-09-14 16:07:21 -0400

    Update vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    emptyOutDir: true
+  },
   server: {
     port: 5173,
     strictPort: true
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: mode !== "production"
   }
-}));
+});
