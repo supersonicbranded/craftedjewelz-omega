7fd911d495015c36725ae6b124526a5ea514c096
supersonicbranded <info@supersonicbranded.com>
2025-09-14 13:12:24 -0400

    Create splash.tsx

---
+++ apps/web/src/screens/splash.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Splash() {
+  return (
+    <div className="splash">
+      <div className="splash-logo">
+        <span className="c">C</span>
+        <span className="j">J</span>
+      </div>
+      <div className="splash-title">CraftedJewelz</div>
+      <div className="splash-sub">Loading creative workspace…</div>
+      <div className="splash-progress" />
+    </div>
+  );
+}
