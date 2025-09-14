e4dc1c87893a71a1437a05be5e6a5d9089858485
supersonicbranded <info@supersonicbranded.com>
2025-09-14 13:05:15 -0400

    Update preload.js

--- apps/web/electron/preload.js
+++ apps/web/electron/preload.js
@@ -1,12 +1,5 @@
-const { contextBridge, ipcRenderer } = require('electron');
+import { contextBridge, ipcRenderer } from "electron";
 
-contextBridge.exposeInMainWorld('electronAPI', {
-  send: (channel, data) => {
-    ipcRenderer.send(channel, data);
-  },
-  receive: (channel, func) => {
-    ipcRenderer.on(channel, (event, ...args) => func(...args));
-  }
+contextBridge.exposeInMainWorld("crafted", {
+  getVersion: async () => ipcRenderer.invoke("app:getVersion")
 });
-
-
