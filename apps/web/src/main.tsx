aa69a277e6432d8805e88624fb6cc069d858488b
supersonicbranded <info@supersonicbranded.com>
2025-09-14 23:12:21 -0400

    Update main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import ReactDOM from "react-dom/client";
+import WelcomeScreen from "./components/WelcomeScreen";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <WelcomeScreen />
   </React.StrictMode>
 );
